f537c1367dc99e367ec758c08e9c65af3bca5c32
Rolando Lopez <rlopeze@gmail.com>
2023-12-13 16:45:17 -0600

    primer commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link rel="preconnect" href="https://fonts.bunny.net">
+        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
+
+        <!-- Styles -->
+        <style>
+            /* ... existing styles ... */
+
+            body {
+                display: flex;
+                align-items: center;
+                justify-content: center;
+                min-height: 100vh; /* Ensure the content takes at least the full height of the viewport */
+                margin: 0;
+            }
+
+            h1 {
+                text-align: center; /* Center the text within the h1 element */
+            }
+        </style>
+    </head>
+    <body >
+                <h1>Bienvenido a mi proyecto en Laravel Rolando López ...</h1>
+    </body>
+</html>
